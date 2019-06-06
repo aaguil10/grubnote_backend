1c20c78a40c6261214786fd532bf6bf5fce85b48
Alejandro Aguilar <alejandroaquilar@gmail.com>
2019-06-06 12:44:30 -0700

    Got it connected to react app

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const express = require('express');
-const cors = require('cors');
-
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors({ origin: true }));
@@ -10,49 +9,45 @@ app.use(cors({ origin: true }));
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
+app.post("/insert", (req, res) => {
+  //var obj = JSON.parse(req.body);
+  var obj = req.body;
 
-app.get('/', function (req, res) {
-  res.send('It Worked!')
+  console.log(req);
+  console.log(req.body);
+  console.log(req.body.title);
+
+  let data = {
+    title: obj.title,
+    subtitle: obj.subtitle,
+    ingredients: obj.ingredients,
+    steps: obj.steps,
+    notes: obj.notes
+  };
+
+  var setDoc = db.collection("recipes").add(data);
+
+  res.send(data);
 });
 
-app.post('/insert', (req, res) => {
-
-
-	var obj = JSON.parse(req.body);
-
-	let data = {
-	  id: obj.id,
-	  title: obj.title,
-	  subtitle: obj.subtitle,
-	  ingredients: obj.ingredients,
-	  steps: obj.steps,
-	  notes: obj.notes
-	};
-
-
-	var setDoc = db.collection('recipes').add(data);
-
-  	res.send(data);  
-});
-
-
-app.get('/getrecipes', function (req, res) {
-	const recipes = [];
-	let collectionGroup = db.collectionGroup('recipes');
-	collectionGroup.get().then(
-		function(collectionSnapshot) {
-  		collectionSnapshot.forEach(function(doc) {
-  			const d = doc.data();
-  			d.id = doc.id;
-	   		recipes.push(d);
-  		});
-  		res.send(recipes);
-   		return true;
-	}).catch(err => {
-		console.log('Error getting document', err);
-		res.send('Error getting document');
-	});
-	
+app.get("/getrecipes", (req, res) => {
+  const recipes = [];
+  let collectionGroup = db.collectionGroup("recipes");
+  collectionGroup
+    .get()
+    .then(collectionSnapshot => {
+      collectionSnapshot.forEach(doc => {
+        const d = doc.data();
+        d.id = doc.id;
+        recipes.push(d);
+      });
+      res.send(recipes);
+      return true;
+    })
+    .catch(err => {
+      console.log("Error getting document", err);
+      res.send("Error getting document");
+    });
 });
 
 exports.recipe = functions.https.onRequest(app);
